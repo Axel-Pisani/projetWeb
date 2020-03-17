03775765d6de30e57a15e1769fdc0348ae73726b
axl-pis <axelpisanipro@gmail.com>
2020-03-17 01:16:49 +0100

    added database framework

---
+++ model/model.js
@@ -0,0 +1,70 @@
+/*
+CREATE TABLE users (
+	id INTEGER PRIMARY KEY AUTOINCREMENT, 
+	name VARCHAR2(100) NOT NULL, 
+	password VARCHAR2(100) NOT NULL, 
+	adresse VARCHAR2(250) NOT NULL, 
+	justificatif VARCHAR2(250) NOT NULL
+);
+
+CREATE TABLE manche (
+	id INTEGER PRIMARY KEY AUTOINCREMENT, 
+	quantity INTEGER CHECK(quantity >= 0), 
+	description VARCHAR2(200), 
+	photo VARCHAR2(200)
+);
+
+CREATE TABLE tuyau (
+	id INTEGER PRIMARY KEY AUTOINCREMENT, 
+	quantity INTEGER CHECK(quantity >= 0), 
+	description VARCHAR2(200), 
+	photo VARCHAR2(200)
+);
+
+CREATE TABLE tête (
+	id INTEGER PRIMARY KEY AUTOINCREMENT, 
+	quantity INTEGER CHECK(quantity >= 0), 
+	description VARCHAR2(200), 
+	photo VARCHAR2(200)
+);
+
+CREATE TABLE goût (
+	id INTEGER PRIMARY KEY AUTOINCREMENT, 
+	quantity INTEGER CHECK(quantity >= 0), 
+	description VARCHAR2(200), 
+	photo VARCHAR2(200)
+);
+
+CREATE TABLE diffuseur (
+	id INTEGER PRIMARY KEY AUTOINCREMENT, 
+	quantity INTEGER CHECK(quantity >= 0), 
+	description VARCHAR2(200), 
+	photo VARCHAR2(200)
+);
+
+
+CREATE TABLE narguile (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	marque VARCHAR2(50) NOT NULL,
+	reference VARCHAR2(100) NOT NULL,
+	quantity INTEGER DEFAULT 0, 
+	noteUser INTEGER CHECK(noteUser >= 0 AND noteUser <= 5), 
+	photo VARCHAR2(150) NOT NULL,
+	idManche INTEGER NOT NULL REFERENCES manche, 
+	idTuyau INTEGER NOT NULL REFERENCES tuyau,
+	idTete INTEGER NOT NULL REFERENCES tete, 
+	idGout INTEGER NOT NULL REFERENCES gout, 
+	idDiffuseur INTEGER NOT NULL REFERENCES diffuseur
+);
+    
+CREATE TABLE location (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    idNarg INTEGER NOT NULL REFERENCES narguile, 
+    idManche INTEGER NOT NULL REFERENCES manche, 
+    idTuyau INTEGER NOT NULL REFERENCES tuyau, 
+    idTete INTEGER NOT NULL REFERENCES tete, 
+    idGout INTEGER DEFAULT NULL REFERENCES gout, 
+    idDiffuseur INTEGER NOT NULL REFERENCES diffuseur
+
+);
+*/
